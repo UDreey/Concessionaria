1169c6b4b7401aa780266081e3a8589be8937dcc
UDreey <108488715+UDreey@users.noreply.github.com>
2025-11-11 20:47:07 -0300

    att-sql

---
+++ SQLQuery5.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO carro (placa, ano, modelo, idMarca)
+values
+('ABC1234', 2020, (select idMarca From marca where nome = 'Toyot')),
+('DEF5678', 2019, (select idMarca From marca where nome ='Honda ')),
+('GHI9012', 2021, (select idMarca From marca where nome ='Ford Focus')),
+('JKL3456', 2018, (select idMarca From marca where nome ='Chevrolet Malibu')),
+('MNO7890', 2022, (select idMarca From marca where nome ='Nissan Sentra'));
