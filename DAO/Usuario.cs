2b4c8cb20387db6f20364e13f6f0d980b1622e6f
UDreey <108488715+UDreey@users.noreply.github.com>
2025-11-11 20:46:19 -0300

    att-2

--- DAO/Usuario.cs
+++ DAO/Usuario.cs
@@ -10,8 +10,9 @@ namespace DAO
     {
         public int id { get; set; }
         public string email { get; set; }
-        public string name { get; set; }
+        public string nome { get; set; }
         public string senha { get; set; }
+        public string login { get; set; }
     }
 
 
